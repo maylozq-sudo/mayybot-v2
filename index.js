9910c79bc9b90e1e9cbe6a683f7bdee9618c9b9d
maylozq-sudo <maylozq@gmail.com>
2026-09-19 07:04:28 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once('ready', () => {
+  console.log(`Bot prêt ! Connecté en tant que ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
